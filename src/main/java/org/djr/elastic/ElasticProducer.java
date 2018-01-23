1d8dd598c27a40752651494e0b5082e07abe65b8
danny <danny.rucker@ironman.local>
2018-01-22 18:43:18 -0600

    updated to elastic 6.1.2
    added transport client to make injectable

--- src/main/java/org/djr/elastic/ElasticProducer.java
+++ src/main/java/org/djr/elastic/ElasticProducer.java
@@ -2,8 +2,13 @@ package org.djr.elastic;
 
 import org.apache.http.HttpHost;
 import org.djr.elastic.properties.ElasticProperties;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +17,8 @@ import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
 import java.util.Properties;
 
 /**
@@ -28,7 +35,7 @@ public class ElasticProducer {
     @ElasticSearch
     public RestHighLevelClient produceElasticSearchHighLevelRestClient(InjectionPoint injectionPoint)
     throws IOException, NullPointerException {
-        log.debug("produceElasticSearchHighLevelRestClient() started");
+        log.trace("produceElasticSearchHighLevelRestClient() started");
         ElasticSearchConfig config = injectionPoint.getAnnotated().getAnnotation(ElasticSearchConfig.class);
         RestHighLevelClient client = null;
         if (null != config) {
@@ -44,6 +51,23 @@ public class ElasticProducer {
             client = new RestHighLevelClient(
                     RestClient.builder(httpHosts)
             );
+        }
+        return client;
+    }
+
+    @Produces
+    @ESTransportClient
+    public Client produceTransportClient(InjectionPoint injectionPoint) {
+        PreBuiltTransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
+        ElasticSearchConfig config = injectionPoint.getAnnotated().getAnnotation(ElasticSearchConfig.class);
+        if (null != config) {
+            String delineator = properties.getProperty(config.delineatorPropertyName());
+            String[] hosts = getStringArray(properties.getProperty(config.hostsPropertyName()), delineator);
+            int[] ports = getIntArray(properties.getProperty(config.portsPropertyName()), delineator);
+            String[] schemes = getStringArray(properties.getProperty(config.schemePropertyName()), delineator);
+            for (int i = 0; i < hosts.length; i++) {
+                client.addTransportAddress(new TransportAddress(InetSocketAddress.createUnresolved(hosts[i], ports[i])));
+            }
         }
         return client;
     }
